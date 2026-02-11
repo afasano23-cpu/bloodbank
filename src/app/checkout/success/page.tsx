3b42f5513b91330baaed7e9f76c45e14f4e9a023
Vibecode Terminal <terminal@vibecodeapp.com>
2026-02-11 19:18:15 +0000

    Sandbox autosave

--- src/app/checkout/success/page.tsx
+++ src/app/checkout/success/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'next'
+import { useEffect, useState } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
 import Link from 'next/link'
 
